81edd8840d5247d810334659ad31fce2320f7352
Markos Girgis <mg.markosgirgis@gmail.com>
2025-07-05 09:19:42 +0300

    feat: add hexagon background component with about page implementation

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { HexagonBackground } from '@/components/animate-ui/backgrounds/hexagon'
+import React from 'react'
+
+const page = () => {
+    return (
+        <HexagonBackground />
+    )
+}
+
+export default page
